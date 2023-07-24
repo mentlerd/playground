e452210aa3ab650b6d6fa0a5fa420448bfa9d9d3
mentlerd <mentlerd@gmail.com>
2023-07-24 20:12:34 +0200

    Fix windows setup

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -3,3 +3,8 @@ if (CMAKE_TOOLCHAIN_FILE)
 endif()
 
 set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/submodules/vcpkg/scripts/buildsystems/vcpkg.cmake")
+
+# Prevent infinite recursion in case the toolchain file is explicitly specified
+if (VCPKG_CHAINLOAD_TOOLCHAIN_FILE STREQUAL CMAKE_TOOLCHAIN_FILE)
+    unset(VCPKG_CHAINLOAD_TOOLCHAIN_FILE)
+endif()
